67a630491f65bcfd275044f27f2b0f3fe9613098
ahmadexe <muahmad710@gmail.com>
2024-06-25 12:57:56 +0500

    feat: checking for user

--- users/main.go
+++ users/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
+	"github.com/ahmadexe/go-grpc/data"
 	pb "github.com/ahmadexe/go-grpc/grpc"
 
 	"google.golang.org/grpc"
@@ -12,6 +14,15 @@ import (
 type userServiceServer struct {
 	pb.UserServiceServer
 }
+
+var (
+	usersSlice = []data.User{
+		{Id: "1", Name: "Alice", Age: 25},
+		{Id: "2", Name: "Bob", Age: 30},
+		{Id: "3", Name: "Foo", Age: 35},
+		{Id: "4", Name: "Bar", Age: 40},
+	}
+)
 
 func main() {
 	lis, err := net.Listen("tcp", ":50051")
@@ -31,4 +42,14 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func (s *userServiceServer) GetUser(ctx context.Context, in *pb.UserRequest) (*pb.UserResponse, error) {
+	for _, user := range usersSlice {
+		if user.Id == in.Id {
+			return &pb.UserResponse{Id: user.Id, Name: user.Name, Age: user.Age}, nil
+		}
+	}
+
+	return nil, nil
 }
